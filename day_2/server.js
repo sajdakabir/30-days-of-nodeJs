ec1e8f73c6519f52605dc503b4cd4dcf9262f6dd
sajdakabir <sajda.kbir.com>
2022-09-06 19:30:59 +0530

    send the html file data to the browser

--- day_2/server.js
+++ day_2/server.js
@@ -1,15 +1,27 @@
 const http=require("http");
+const fs=require("fs");
 const server= http.createServer((req,res)=>{
     console.log("request has been made from browser to server");
-    console.log(req.method);
-    console.log(req.url);
-    res.setHeader("Content-Type","text/html");
-    res.write("<h1>Hello, from sajda</h1>");
-    res.write("<h2>what's going on</h2>");
-    res.end();
+    // console.log(req.method);
+    // console.log(req.url);
+   
     // res.setHeader("Content-Type","text/plain");
     // res.write("Hello, from sajda");
     // res.end();
+    res.setHeader("Content-Type","text/html");
+    // res.write("<h1>Hello, from sajda</h1>");
+    // res.write("<h2>what's going on</h2>");
+    // res.end();
+    fs.readFile("./views/index.html",(err,fileData)=>{
+        if(err){
+            console.log(err);
+        }else{
+            // res.write(fileData);
+            res.end(fileData);
+        }
+    })
+    
+
 });
 // port, host,callback function
 server.listen(3000,'localhost',()=>{
