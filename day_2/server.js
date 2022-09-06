0060642bd5c49401dd83f8d835b282d21817604e
sajdakabir <sajda.kbir.com>
2022-09-06 19:16:14 +0530

    send some response to the browser

--- day_2/server.js
+++ day_2/server.js
@@ -1,6 +1,15 @@
 const http=require("http");
 const server= http.createServer((req,res)=>{
     console.log("request has been made from browser to server");
+    console.log(req.method);
+    console.log(req.url);
+    res.setHeader("Content-Type","text/html");
+    res.write("<h1>Hello, from sajda</h1>");
+    res.write("<h2>what's going on</h2>");
+    res.end();
+    // res.setHeader("Content-Type","text/plain");
+    // res.write("Hello, from sajda");
+    // res.end();
 });
 // port, host,callback function
 server.listen(3000,'localhost',()=>{
