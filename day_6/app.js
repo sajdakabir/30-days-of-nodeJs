dce790ea6be5f9ccbe0675c32dc06691d4dbbd5f
sajdakabir <sajda.kbir.com>
2022-09-17 20:50:52 +0530

    create a user in db

--- day_6/app.js
+++ day_6/app.js
@@ -13,3 +13,40 @@ mongoose.connect(db_link)
 .catch(function(err){
     console.log(err);
 });
+
+const userSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:8
+    },
+    confirmPassword:{
+        type:String,
+        required:true,
+        minLength:8
+    }
+});
+
+// model
+
+const userModel=mongoose.model('userModel',userSchema);
+
+ (async function createUser(){
+    let user={
+        name:'sajda kabir',
+        email:'abd@gmail.com',
+        password:'12345678',
+        confirmPassword:'12345678'
+    };
+    let data= await userModel.create(user);
+    console.log(data);
+})
