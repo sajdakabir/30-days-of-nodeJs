a0ef8ca1a998e404b163f4402b74f9cf4abd4dec
sajdakabir <sajda.kbir.com>
2022-09-22 19:02:03 +0530

    userController all functions done

--- foodApp/controllers/userController.js
+++ foodApp/controllers/userController.js
@@ -1,19 +1,86 @@
-const userModel=require('../models/userModel');
+const userModel = require('../models/userModel');
 
 
-module.exports.updateUser=function updateUser(req,res){
-
+module.exports.updateUser =async function updateUser(req, res) {
+    try{
+        let id=req.params.id;
+        let user= await userModel.findById(id);
+        let dataToBeUpdated=req.body;
+        if(user){
+            const keys=[];
+            for(let key in dataToBeUpdated){
+                keys.push(key);
+            }
+            for(let i=0;i<keys.length;i++){
+                user[keys[i]]=dataToBeUpdated[keys[i]];
+            }
+            const updateData=await user.save();
+            res.json({
+                message:"data updated successfully",
+                data:user
+            });
+        }else{
+            res.json({
+                message:"user not found"
+            })
+        }
+    }catch(err){
+        res.json({
+            message:err.message
+        })
+    }
 }
 
-module.exports.deleteUser=function deleteUser(req,res){
+module.exports.deleteUser =async function deleteUser(req, res) {
+    try{
+        let id=req.params.id;
+        let user=await userModel.findByIdAndDelete(id);
+        if(!user){
+            res.json({
+                message:"data has been deleted",
+                data:user
+            })
+        }
 
+    }catch(err){
+        res.json({
+            message:err.message
+        });
+    }
 }
 
-module.exports.getUser=function getUser(req,res){
-
+module.exports.getUser = async function getUser(req, res) {
+    try {
+        let id = req.params.id;
+        let user = await userModel.findById(id);
+        if (user) {
+            return res.json(user);
+        } else {
+            return res.json({
+                message: 'user not found'
+            });
+        }
+    } catch (err) {
+        res.json({
+            message: err.message
+        })
+    }
 }
 
-module.exports.getAllUser=function getAllUser(req,res){
-
+module.exports.getAllUser = async function getAllUser(req, res) {
+try{
+    let users=await userModel.find();
+    if(users){
+        res.json({
+            message:"users received",
+            data:users
+        });
+    }
+    res.send("user id received");
+}catch(err){
+    res.json({
+        message:err.message
+    })
+}
 }
 
